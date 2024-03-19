354f9e25db238bd6cbd8512732c43314ddafea5d
Marina Kuratova <marina.kuratova@sbermarket.ru>
2024-03-19 16:10:39 +0300

    review fixes

--- precode.sh
+++ precode.sh
@@ -18,31 +18,30 @@ touch dir2/empty
 # #!/bin/bash
 # echo "$1, привет!"
 echo '#!/bin/bash
-echo "$*, привет!"' > dir2/hello.sh
+echo "$1, привет!"' > dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 chmod 764 dir2/hello.sh
 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
-ls dir2/ > dir2/list.txt
+ls dir2 > dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-cp -r -T dir2 dir3/dir4
+cp -R dir2/* dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
-find ./ -name "*.txt" > dir1/summary.txt
+find . -name "*.txt" > dir1/summary.txt
 
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
 cat dir2/list.txt  >> dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
-NAME='Всем студентам'
+export NAME='Всем студентам'
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-./dir2/hello.sh $NAME >> dir1/summary.txt
-
+./dir2/hello.sh "$NAME" >> dir1/summary.txt
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
 mv dir1/summary.txt 'Практическое задание.txt'
 
